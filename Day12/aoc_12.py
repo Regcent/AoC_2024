a7a912011c6548b522cc4f6858fdcdebfb67a1fe
Regcent <vincent.regnard.de.lagny@gmail.com>
2024-12-16 17:21:43 +0100

    12 works

--- Day12/aoc_12.py
+++ Day12/aoc_12.py
@@ -40,7 +40,9 @@ def your_script(raw_data: str) -> Union[int, str, float, bool]:
 def find_price(grid: list, explored: dict, next_case: tuple, max_x: int, max_y: int) -> int:
     perimeter = 0
     area = 0
-    sides = 0
+    border = []
+    segments_v = dict()
+    segments_h = dict()
     to_check = [next_case]
     print(f"Checking {grid[next_case[1]][next_case[0]]}")
     counted = set()
@@ -48,20 +50,52 @@ def find_price(grid: list, explored: dict, next_case: tuple, max_x: int, max_y: 
     while to_check:
         curr = to_check.pop(0)
         area += 1
-        explored[curr[1]].add(curr[0])
-        neigh = neighbors(curr[0], curr[1], max_x, max_y)
-        perimeter += 4 - len(neigh) # Automatically add the "empty" neighbors as perimeter
-        for neighbor in neigh:
+        x = curr[0]
+        y = curr[1]
+        explored[y].add(x)
+        for neighbor in [(x-1, y), (x+1, y)]:
             # Add same neighbors to "counted" to avoid issues - other neighbors count as perimeter
             if neighbor in counted:
+                continue
+            if neighbor[0] < 0 or neighbor[0] > max_x:
+                border.append(neighbor)
+                if (x, neighbor[0]) not in segments_h:
+                    segments_h[(x, neighbor[0])] = [y]
+                else:
+                    segments_h[(x, neighbor[0])].append(y)
                 continue
             if grid[curr[1]][curr[0]] == grid[neighbor[1]][neighbor[0]]:
                 to_check.append(neighbor)
                 counted.add(neighbor)
             else:
-                perimeter += 1
-    print(perimeter, area)
-    return perimeter * area
+                border.append(neighbor)
+                if (x, neighbor[0]) not in segments_h:
+                    segments_h[(x, neighbor[0])] = [y]
+                else:
+                    segments_h[(x, neighbor[0])].append(y)
+        for neighbor in [(x, y-1), (x, y+1)]:
+            # Add same neighbors to "counted" to avoid issues - other neighbors count as perimeter
+            if neighbor in counted:
+                continue
+            if neighbor[1] < 0 or neighbor[1] > max_y:
+                border.append(neighbor)
+                if (y, neighbor[1]) not in segments_v:
+                    segments_v[(y, neighbor[1])] = [x]
+                else:
+                    segments_v[(y, neighbor[1])].append(x)
+                continue
+            if grid[curr[1]][curr[0]] == grid[neighbor[1]][neighbor[0]]:
+                to_check.append(neighbor)
+                counted.add(neighbor)
+            else:
+                border.append(neighbor)
+                if (y, neighbor[1]) not in segments_v:
+                    segments_v[(y, neighbor[1])] = [x]
+                else:
+                    segments_v[(y, neighbor[1])].append(x)
+    sides = count_sides(segments_h, segments_v)
+    print(area, len(border), sides)
+    return area, len(border), sides
 
 def find_next_case(explored: dict, max_x: int) -> tuple:
     for key in explored:
@@ -71,17 +105,24 @@ def find_next_case(explored: dict, max_x: int) -> tuple:
                     return (i, key)
     return None
 
-def neighbors(x: int, y: int, max_x: int, max_y: int)-> list:
-    neighbors = list()
-    if x > 0:
-        neighbors.append((x - 1,y))
-    if x < max_x:
-        neighbors.append((x + 1, y))
-    if y > 0:
-        neighbors.append((x, y - 1))
-    if y < max_y:
-        neighbors.append((x, y + 1))
-    return neighbors
+def count_sides(segments_h: dict, segments_v: dict) -> int:
+    sides = 0
+    for y in segments_h:
+        sides += 1
+        if len(segments_h[y]) > 1:
+            segments_h[y].sort()
+            for i in range(len(segments_h[y]) - 1):
+                if segments_h[y][i + 1] - segments_h[y][i] != 1:
+                    sides += 1
+    for x in segments_v:
+        sides += 1
+        if len(segments_v[x]) > 1:
+            segments_v[x].sort()
+            for i in range(len(segments_v[x]) - 1):
+                if segments_v[x][i + 1] - segments_v[x][i] != 1:
+                    sides += 1
+    print(segments_h, segments_v)
+    return sides
 
 if __name__ == "__main__":
     print(run_script("input.txt"))
