5feb76dcae52dfbdee96eb19d9d36d269452043f
Regcent <vincent.regnard.de.lagny@gmail.com>
2024-12-19 17:50:33 +0100

    16 Works, but the new way to solve definitely needs optimizing

--- Day16/aoc_16.py
+++ Day16/aoc_16.py
@@ -48,49 +48,78 @@ def your_script(raw_data: str) -> Union[int, str, float, bool]:
             else:
                 row.append(False)
         grid.append(deepcopy(row))
-    print(f"Part 1: {part_1(grid, start, end)}")
+    finish_nodes = solve(grid, start, end)
+    print(f"Part 1: {finish_nodes[0][0]}")
+    spots = set()
+    for node in finish_nodes:
+        for pos in node[2]:
+            spots.add(pos)
+    print(f"Part 2: {len(spots)}")
     return 0
 
-def part_1(grid: list, start: tuple, end: tuple) -> None:
+def solve(grid: list, start: tuple, end: tuple) -> None:
     pq = PriorityQueue()
-    pq.put((0, start))
-    explored = set()
-    explored.add((start[0], start[1]))
-    while True:
+    pq.put((0, start, [(start[0], start[1])]))
+    explored = dict()
+    finish_nodes = []
+    min_score = 1000000
+    while pq.qsize():
         curr = pq.get()
         score = curr[0]
+        if score > min_score:
+            continue
         (x, y, direction) = curr[1]
-        explored.add((x, y))
+        if curr[1] not in explored:
+            explored[curr[1]] = score
+        elif explored[curr[1]] < score:
+            continue
         if (x, y) == end:
-            return score
+            if score < min_score:
+                print("Problem if it appears more than once")
+                min_score = score
+            finish_nodes.append(curr)
+            continue
         if direction == Dir.NORTH:
-            if grid[y - 1][x] and (x, y - 1) not in explored:
-                pq.put((score + 1, (x, y - 1, Dir.NORTH)))
-            if grid[y][x + 1] and (x + 1, y) not in explored:
-                pq.put((score + 1001, (x + 1, y, Dir.EAST)))
-            if grid[y][x - 1] and (x - 1, y) not in explored:
-                pq.put((score + 1001, (x - 1, y, Dir.WEST)))
+            if grid[y - 1][x]:
+                if check(explored, (x, y - 1, Dir.NORTH),  score + 1):
+                    pq.put((score + 1, (x, y - 1, Dir.NORTH), deepcopy(curr[2]) + [(x, y - 1)]))
+            if check(explored, (x, y, Dir.EAST), score + 1000):
+                pq.put((score + 1000, (x, y, Dir.EAST), deepcopy(curr[2])))
+            if check(explored, (x, y, Dir.WEST), score + 1000):
+                pq.put((score + 1000, (x, y, Dir.WEST), deepcopy(curr[2])))
         if direction == Dir.EAST:
-            if grid[y][x + 1] and (x + 1, y) not in explored:
-                pq.put((score + 1, (x + 1, y, Dir.EAST)))
-            if grid[y - 1][x] and (x, y - 1) not in explored:
-                pq.put((score + 1001, (x, y - 1, Dir.NORTH)))
-            if grid[y + 1][x] and (x, y + 1) not in explored:
-                pq.put((score + 1001, (x, y + 1, Dir.SOUTH)))
+            if grid[y][x + 1]:
+                if check(explored,(x + 1, y, Dir.EAST), score + 1):
+                    pq.put((score + 1, (x + 1, y, Dir.EAST), deepcopy(curr[2]) + [(x + 1, y)]))
+            if check(explored, (x, y, Dir.NORTH), score + 1000):
+                pq.put((score + 1000, (x, y, Dir.NORTH), deepcopy(curr[2])))
+            if check(explored, (x, y, Dir.SOUTH), score + 1000):
+                pq.put((score + 1000, (x, y, Dir.SOUTH), deepcopy(curr[2])))
         if direction == Dir.SOUTH:
-            if grid[y + 1][x] and (x, y + 1) not in explored:
-                pq.put((score + 1, (x, y + 1, Dir.SOUTH)))
-            if grid[y][x + 1] and (x + 1, y) not in explored:
-                pq.put((score + 1001, (x + 1, y, Dir.EAST)))
-            if grid[y][x - 1] and (x - 1, y) not in explored:
-                pq.put((score + 1001, (x - 1, y, Dir.WEST)))
+            if grid[y + 1][x]:
+                if check(explored, (x, y + 1, Dir.SOUTH),  score + 1):
+                    pq.put((score + 1, (x, y + 1, Dir.SOUTH), deepcopy(curr[2]) + [(x, y + 1)]))
+            if check(explored, (x, y, Dir.EAST), score + 1000):
+                pq.put((score + 1000, (x, y, Dir.EAST), deepcopy(curr[2])))
+            if check(explored, (x, y, Dir.WEST), score + 1000):
+                pq.put((score + 1000, (x, y, Dir.WEST), deepcopy(curr[2])))
         if direction == Dir.WEST:
-            if grid[y][x - 1] and (x - 1, y) not in explored:
-                pq.put((score + 1, (x - 1, y, Dir.WEST)))
-            if grid[y - 1][x] and (x, y - 1) not in explored:
-                pq.put((score + 1001, (x, y - 1, Dir.NORTH)))
-            if grid[y + 1][x] and (x, y + 1) not in explored:
-                pq.put((score + 1001, (x, y + 1, Dir.SOUTH)))
+            if grid[y][x - 1]:
+                if check(explored,(x - 1, y, Dir.WEST), score + 1):
+                    pq.put((score + 1, (x - 1, y, Dir.WEST), deepcopy(curr[2]) + [(x - 1, y)]))
+            if check(explored, (x, y, Dir.NORTH), score + 1000):
+                pq.put((score + 1000, (x, y, Dir.NORTH), deepcopy(curr[2])))
+            if check(explored, (x, y, Dir.SOUTH), score + 1000):
+                pq.put((score + 1000, (x, y, Dir.SOUTH), deepcopy(curr[2])))
+    return finish_nodes
+
+def check(explored: dict, new: tuple, new_score: int) -> None:
+    if new in explored:
+        if new_score > explored[new]:
+            return False
+        else:
+            print("Change needed")
+    return True
 
 if __name__ == "__main__":
     print(run_script("input.txt"))
